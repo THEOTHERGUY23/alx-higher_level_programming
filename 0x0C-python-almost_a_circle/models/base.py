e2aabfc170b1a3c573dcc1cae8e7d72e8e200a0d
THEOTHERGUY23 <mzimnguni23@gmail.com>
2024-02-14 02:41:15 -0800

    circle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''Module for Base class.'''
+from json import dumps, loads
+import csv
+
+
+class Base:
+    '''A representation of the base of our OOP hierarchy.'''
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        '''Constructor.'''
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''Jsonifies a dictionary so it's quite rightly and longer.'''
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        else:
+            return dumps(list_dictionaries)
